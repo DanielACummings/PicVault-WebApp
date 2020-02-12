498a76a05f79a4c797fc0628cc25c06d982ae598
Daniel <danielac@protonmail.com>
2020-02-12 16:35:59 -0700

    saving progress

--- wwwsrc/src/components/VaultComp.vue
+++ wwwsrc/src/components/VaultComp.vue
@@ -1,14 +1,16 @@
 <template>
-	<div class="vault-comp col-4 pb-3">
-		<div class="card" style="width: 18rem;">
-			<div class="card-body">
-				<h5 class="card-title">{{vaultProp.name}}</h5>
-				<p class="card-text">{{vaultProp.description}}</p>
-				<button type="submit" class="btn btn-primary">View</button>
-				<button @click="deleteVault" type="delete" class="btn btn-danger">Delete</button>
+	<router-link :to="{name: 'vault', params: {id: vaultProp.id}}">
+		<div class="vault-comp col-4 pb-3">
+			<div class="card" style="width: 18rem;">
+				<div class="card-body">
+					<h5 class="card-title">{{vaultProp.name}}</h5>
+					<p class="card-text">{{vaultProp.description}}</p>
+					<button type="submit" class="btn btn-primary">View</button>
+					<button @click="deleteVault" type="delete" class="btn btn-danger">Delete</button>
+				</div>
 			</div>
 		</div>
-	</div>
+	</router-link>
 </template>
 
 <script>
--- wwwsrc/src/views/Vault.vue
+++ wwwsrc/src/views/Vault.vue
@@ -2,7 +2,7 @@
 	<div class="vault container">
 		<div class="row">
 			<div class="col-12">
-				<h1>Vault Name</h1>
+				<h1>{{}}</h1>
 			</div>
 		</div>
 		<div class="row">
