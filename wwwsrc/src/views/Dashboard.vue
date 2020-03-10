5c40ecaf5dd137d140ec3cd1c3b042749498fb99
Daniel <danielac@protonmail.com>
2020-03-10 10:40:06 -0600

    adds create vault modal

--- wwwsrc/src/views/Dashboard.vue
+++ wwwsrc/src/views/Dashboard.vue
@@ -16,13 +16,49 @@
 					<button type="submit" class="btn btn-primary">Submit</button>
 				</form>
 			</div>
+			<!-- Create vault modal -->
 			<div class="col-6">
-				<h5>Create a Vault</h5>
-				<form @submit.prevent="createVault">
-					<input type="text" v-model="newVault.name" required placeholder="Vault name" />
-					<input type="text" v-model="newVault.description" placeholder="Description" />
-					<button type="submit" class="btn btn-primary">Submit</button>
-				</form>
+				<button
+					type="button"
+					class="btn btn-primary"
+					data-toggle="modal"
+					data-target="#exampleModal"
+					data-whatever="@getbootstrap"
+				>Create Vault</button>
+
+				<div
+					class="modal fade"
+					id="exampleModal"
+					tabindex="-1"
+					role="dialog"
+					aria-labelledby="exampleModalLabel"
+					aria-hidden="true"
+				>
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLabel">New Vault</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div class="modal-body">
+								<form @submit.prevent="createVault">
+									<div class="form-group">
+										<label for="vault-name" class="col-form-label">Name:</label>
+										<input v-model="newVault.name" required type="text" class="form-control" id="vault-name" />
+									</div>
+									<div class="form-group">
+										<label for="message-text" class="col-form-label">Description:</label>
+										<input v-model="newVault.description" class="form-control" id="message-text" />
+									</div>
+									<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+									<button type="submit" class="btn btn-primary">Submit</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="row">
